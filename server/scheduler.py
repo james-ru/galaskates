51ad3dbc0ab77bea771d572e58d6d3afbd765b33
James <38256531+james-ru@users.noreply.github.com>
2018-09-26 17:28:13 +0200

    master

--- server/scheduler.py
+++ server/scheduler.py
@@ -1,6 +1,7 @@
-from itertools import cycle
 import time
 
+# Array of instructions:
+# mode | red | green | blue | interval (until next instruction)
 scheduler = [
 	[1, 255, 0, 0, 0.5],
 	[1, 0, 255, 0, 0.5],
@@ -10,11 +11,12 @@ scheduler = [
 	[1, 255, 255, 0, 0.5],
 ]
 
+# Edit string.txt with a new instruction
 def publish(mode, r, g, b):
 	f = open("string.txt", "w")
 	f.write(mode + "," + r + "," + g + "," + b)
 
-
+# Send off every instruction depending on their respective interval to publish()
 while True:
 	for schedule in scheduler:
 		mode = str(schedule[0])
--- server/webserv.py
+++ server/webserv.py
@@ -2,18 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-	return "Hello World!"
-
-@app.route("/r")
-def r():
+def color():
 	file = open("string.txt", "r")	
 	return file.read()
-
-@app.route("/g")
-def g():
-	return '100'
-
-@app.route("/b")
-def b():
-	return '255'
